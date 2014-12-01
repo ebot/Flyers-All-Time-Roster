6023cdfb0f921f2846db0f2d1e5e230d859178f2
Ed Botzum <contact@edbotz.us>
2014-12-01 00:57:07 -0500

    2014-2015 Flyer Numbers Added

--- script/make_csv.rb
+++ script/make_csv.rb
@@ -1,27 +1,35 @@
 #!/usr/bin/env ruby -wKU
 
-input = File.new 'flyer_numbers-2010-2011.txt', 'r'
-out1 = File.new 'flyer_numbers-2010-2011.csv', 'w'
+input = File.new 'flyer_numbers-2014-2015.txt', 'r'
+out1 = File.new 'flyer_numbers-2014-2015.csv', 'w'
 out2 = File.new 'flyer_numbers.csv', 'w'
 
-input.each_line do |line|
-  player = line.chomp.split('|')
-  position = player[1]
-  name     = player[2]
-  years    = player[3]
-  unless player[0].index(',').nil?
-    numbers = player[0].split(', ')
-    numbers.each do |number|
-      out1 << number + ';"' + position + '";"' + name + '";"' + years + "\"\n"
-      out2 << number + ';"' + position + '";"' + name + '";"' + years + "\"\n"
+last_line = ''
+begin
+  input.each_line do |line|
+    last_line = line
+    player = line.chomp.split('|')
+    position = player[1]
+    name     = player[2]
+    years    = player[3]
+    puts "Reading #{name}"
+    unless player[0].index(',').nil?
+      numbers = player[0].split(', ')
+      numbers.each do |number|
+        out1 << number + ';"' + position + '";"' + name + '";"' + years + "\"\n"
+        out2 << number + ';"' + position + '";"' + name + '";"' + years + "\"\n"
+      end
+    else
+      out1 << player[0] + ';"' + position + '";"' + name + '";"' + years + "\"\n"
+      out2 << player[0] + ';"' + position + '";"' + name + '";"' + years + "\"\n"
+      out1.flush
+      out2.flush
     end
-  else
-    out1 << player[0] + ';"' + position + '";"' + name + '";"' + years + "\"\n"
-    out2 << player[0] + ';"' + position + '";"' + name + '";"' + years + "\"\n"
-    out1.flush
-    out2.flush
   end
-end
 
-out1.close
-out2.close
+  out1.close
+  out2.close
+rescue Exception => e
+  puts last_line
+  puts e.message
+end
--- script/make_mongodb.rb
+++ script/make_mongodb.rb
@@ -1,29 +1,45 @@
 #!/usr/bin/env ruby
 require 'mongo'
+require 'uri'
 
-m = Mongo::Connection.new
-m.drop_database('flyers')
+@mongo_url = 'mongodb://user:pass@server:port/db'
 
-db = Mongo::Connection.new("localhost").db("flyers")
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(@mongo_url)
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.password.nil?)
+  @db_connection
+end
+
+db = get_connection
+
+puts 'Dropping and Recreating Players Collection'
+
 players = db["players"]
+players.drop
+players = db["players"]
+
+puts 'Collection Created, Adding Players...'
 
 input = File.new 'flyer_numbers.csv', 'r'
 input.each_line do |line|
   line = line.gsub('"', '').chomp
   info = line.split(';')
-  
+
   player = {
     :name => info[2],
     :position => info[1],
     :years => info[3],
     :numbers => [info[0].to_i]
   }
-  
+
   mongo_player = players.find_one(
                   "name" => player[:name],
                   "position" => player[:position],
                   "years" => player[:years] )
-  
+
   if mongo_player.nil?
     players.insert( player )
   else
@@ -31,3 +47,5 @@ input.each_line do |line|
     players.save( mongo_player )
   end
 end
+
+puts 'Done'
